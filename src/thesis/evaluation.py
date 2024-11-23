fixed off by one video bug

src/thesis/evaluation.py
@@ -151,16 +151,29 @@ def fov_video_vdp(
     return a.item(), heatmap
 
 
+# =============================================================================================== #
+#                                    Evaluation Function                                          #
+# =============================================================================================== #
+
+
 def evaluate(
     gt_videos_directory: str,
     pred_videos_directory: str,
     output_directory: str | None = None,
     sequences: list[str] | list[int] | None = None,
     device: torch.device | str = 'cpu',
+    cut_lower_n_pixels: int = 350,
 ) -> None:
     """
-    Evaluate videos.
-
+    Args:
+        gt_videos_directory: Directory containing ground truth videos.
+        pred_videos_directory: Directory containing predicted videos.
+        output_directory: Directory to save evaluation results.
+        sequences: List of sequences to evaluate.
+        device: Device to use for computation.
+        cut_lower_n_pixels: Number of pixels to cut from the lower part of the video. The
+            segmentation masks don't always reach to the very bottom of the screen, which isn't
+            ideal. The neck isn't really part of our evaluation anyways, so we can cut it off.
     """
 
     computer = _EvaluationComputer()
@@ -194,9 +207,9 @@ def evaluate(
             sequence = f'sequence_{sequence}.mp4'
         print('Loading videos')
         gt_video = load_video(f'{gt_videos_directory}/{sequence}')
-        gt_video = torch.from_numpy(gt_video)
+        gt_video = torch.from_numpy(gt_video)[:, :-cut_lower_n_pixels]
         pred_video = load_video(f'{pred_videos_directory}/{sequence}')
-        pred_video = torch.from_numpy(pred_video)
+        pred_video = torch.from_numpy(pred_video)[:, :-cut_lower_n_pixels]
         print('Done!')
         results_sequence = computer(gt_video, pred_video, device)
         results['psnr'].append(results_sequence['psnr'])
@@ -237,6 +250,11 @@ def evaluate(
     print(f'Average L1: {results["l1_avg"]}')
     print(f'Average Foveated Video Quality Prediction: {results["fov_video_vdp_avg"]}')
     print(f'Results saved to {output_path}')
+
+
+# =============================================================================================== #
+#                                    Graphing Function                                            #
+# =============================================================================================== #
 
 
 def graph_evaluation(dir_path: str) -> None:
@@ -439,14 +457,14 @@ def plot_all_visualizations(dir_path: str):
 
 if __name__ == '__main__':
 
-    gt_path = 'tmp/gt/masked'
-    pred_path = 'tmp/pred/2dgs_full_res_500k_overnight_rigging_large_lpips/flame'
+    gt_dir = 'tmp/gt/masked'
+    pred_dir = 'tmp/pred/2dgs_full_res_500k_overnight_rigging_large_lpips/flame'
     # evaluate(
-    #     gt_path=gt_path,
-    #     pred_path=pred_path,
+    #     gt_path=gt_dir,
+    #     pred_path=pred_dir,
     #     sequences=[i for i in range(80, 102)],
     #     device='cuda',
     # )
-    graph_evaluation(pred_path)
-
-    # test why I can't launch it directly, but can call it from another script
+    for key, value in plot_all_visualizations(pred_dir).items():
+        print(key)
+        value.show()

src/thesis/render_video.py
@@ -38,15 +38,15 @@ from thesis.video_utils import add_audio
 
 @torch.no_grad()
 def process_audio(
-        audio_path: str,
-        fps: int = 30,  # ahhh it's not actually 30, just very close to it
+    audio_path: str,
+    n_frames: int | None,
 ) -> Float[torch.Tensor, 'time 1024']:
     """
     Loads the audio from disk and processes it to extract features.
 
     Args:
         audio_path: Path to the audio file.
-        fps: Frames per second. Defaults to 30.
+        n_frames: Number of frames to process.
         device: Device to use. Defaults to 'cuda'.
 
     Returns:
@@ -70,8 +70,9 @@ def process_audio(
         audio, sr = sf.read(audio_path)
     audio = librosa.resample(audio, orig_sr=sr, target_sr=16_000)
     sr = 16_000
-    audio_length = len(audio) / sr  # in seconds
-    n_frames = int(audio_length * fps)
+    if n_frames is None:
+        audio_length = len(audio) / sr  # in seconds
+        n_frames = int(audio_length * 30.2)
     audio_features = process_sequence_interpolation(
         audio=audio,
         sampling_rate=sr,
@@ -430,9 +431,8 @@ def render_gt_video(
 
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         out.write(image)
-
     out.release()
-    add_audio(
+    add_audio(  # adds a frame here for some reason in edge cases
         video_path=output_path,
         audio_path=audio_path,
         fps=30,
@@ -477,7 +477,12 @@ def main(
     """
 
     # Set up
-    audio_features = process_audio(audio_path)
+    if flame_params_sequence is not None:
+        sm = SequenceManager(flame_params_sequence)
+        n_frames = sm.flame_params[:].expr.shape[0]
+    else:
+        n_frames = None
+    audio_features = process_audio(audio_path, n_frames)
     splats = DynamicGaussianSplatting.load_from_checkpoint(
         gaussian_splats_checkpoint_path, ckpt_path=gaussian_splats_checkpoint_path)
     splats.eval()
@@ -554,15 +559,15 @@ def main(
     )
 
 
-# ==================================================================================== #
-#                                       MAIN                                           #
-# ==================================================================================== #
+# ============================================================================================== #
+#                                       MAIN FUNCTION                                            #
+# ============================================================================================== #
 
 if __name__ == '__main__':
     # Arguments
     mode: Literal['flame', 'audio'
                   'gt'] = 'flame'
-    sequence: int | None = 100
+    sequence: int | None = 85
     audio_path: str | None = None  # 'tmp/german_test.m4a'
     gaussian_splats_checkpoint: str = 'tb_logs/dynamic_gaussian_splatting/2dgs_full_res_500k_overnight_rigging_large_lpips/version_0/checkpoints/epoch=7-step=800000.ckpt'  # noqa
     # audio_to_flame_checkpoint: str | None = None